48f34c5ef3adf18805c8f8c90ad5b8dd18ca3c03
gimseung-yeon <rlamd0412@gmail.com>
2022-04-21 17:05:51 +0900

    Initial commit

---
+++ src/main/java/me/tom/backendtddex/BackEndTddExApplication.java
@@ -0,0 +1,13 @@
+package me.tom.backendtddex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndTddExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndTddExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/tom/backendtddex/BackEndTddExApplicationTests.java
@@ -0,0 +1,13 @@
+package me.tom.backendtddex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndTddExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
