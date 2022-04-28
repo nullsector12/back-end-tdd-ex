0dd4cee9a45a00a558a46391c913751950ca6c01
gimseung-yeon <rlamd0412@gmail.com>
2022-04-28 18:48:05 +0900

    환경설정 파일 업데이트
    1. tomcat -> undertow 로 변경
    2. jpa, mysql, querydsl 설정 추가
    3. gson, mapstruct 추가

---
+++ src/main/java/me/tom/backendtddex/infraStructure/configuration/QueryDslConfig.java
@@ -0,0 +1,21 @@
+package me.tom.backendtddex.infraStructure.configuration;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Configuration
+public class QueryDslConfig {
+    @PersistenceContext(unitName = "commandEntityManagerFactory")
+    private EntityManager commandEntityManger;
+
+    @Primary
+    @Bean
+    public JPAQueryFactory commandQueryFactory() {
+        return new JPAQueryFactory(commandEntityManger);
+    }
+}
